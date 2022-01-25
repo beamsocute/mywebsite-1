c2ffbb37031718ff3f2f25f569c8edc4f8bd975c
Thitira <97937737+Thitira@users.noreply.github.com>
2022-01-25 15:03:46 +0700

    Update page1.php

--- page1.php
+++ page1.php
@@ -2,7 +2,7 @@
         <head>
                 <title>งานที่ 1</title>
         </head>
-        <body bgcolor="#FFBBDA">
+        <body bgcolor="#89CFEF">
                 <h1>ประวัติส่วนตัว</h1>
                 <br>
                 <b>ชื่อ;</b> <i>ฐิติ​รัตน์​ อ่อน​ศรี​</i>
