bae40e6da99da7bd0e0bc1b20eed4d73558685c5
Thitira <97937737+Thitira@users.noreply.github.com>
2022-02-08 19:36:27 +0700

    Update table.php

--- table.php
+++ table.php
@@ -13,9 +13,9 @@
       </tr>
       <tr>
         <td>1</td>
-        <td>มารี เออเจนี เลอเอย์</td>
+        <td>กันต์พิมุกต์ ภูวกุล</td>
         <td>ส้ม</td>
-        <td><img src="https://s.isanook.com/jo/0/rp/r/w728/ya0xa0m1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL2pvLzAvdWQvNDg0LzI0MjA4MTMvejIuanBn.jpg" width="130px"></td>
+        <td><img src="https://pbs.twimg.com/profile_images/1481642811749404682/NXBKpa7n_400x400.jpg" width="130px"></td>
         <td><a href="https://twitter.com/zommarie?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a></td>
       </tr>
       <tr>
