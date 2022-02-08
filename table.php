9397afff66f19eebe549aa6c5e2519fbc49c2911
Thitira <97937737+Thitira@users.noreply.github.com>
2022-02-08 14:29:45 +0700

    Update table.php

--- table.php
+++ table.php
@@ -1,1 +1,20 @@
 
+<html>
+  <head>
+    <title>ตารางของฉัน</title>
+  </head>
+  <body>
+    <table border="26">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ - นามสกุล</th>
+        <th>ชื่อเล่น</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>ฐิติ​รัตน์​ อ่อน​ศรี​</td>
+        <td>น้ำตาล</td>
+      </tr>  
+    </table>
+  </body> 
+</html>
