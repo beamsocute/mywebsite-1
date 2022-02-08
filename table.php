aaf11d9cb223d85a4de970145def2aeec11b845f
Thitira <97937737+Thitira@users.noreply.github.com>
2022-02-08 20:37:21 +0700

    Update table.php

--- table.php
+++ table.php
@@ -1,1 +1,58 @@
-
+<html>
+  <head>
+    <title>ตารางของฉัน</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ - นามสกุล</th>
+        <th>ชื่อเล่น</th>
+        <th>รูป</th>
+        <th>ทวิตเตอร์</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>กันต์พิมุกต์ ภูวกุล</td>
+        <td>แบมแบม</td>
+        <td><img src="https://pbs.twimg.com/profile_images/1481642811749404682/NXBKpa7n_400x400.jpg" width="130px"></td>
+        <td><a href="https://twitter.com/BamBam1A?t=enI4UnHsdi0t5P3TAXCQIQ&s=09">twitter</a></td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>พุฒิพงศ์ อัสสรัตนกุล</td>
+        <td>บิวกิ้น</td>
+        <td><img src="https://static.thairath.co.th/media/4DQpjUtzLUwmJZZSC4MkkJTjPezKgdjFBUo7344aSuqc.jpg" width="130px"></td>
+        <td><a href="https://twitter.com/bbillkin?t=l3xOuXzb9f-yoti_vRNCjA&s=09">twitter</a></td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>กฤษฏ์ อำนวยเดชกร</td>
+        <td>พีพี</td>
+        <td><img src="https://s.isanook.com/ca/0/ui/280/1400284/81042589_470613516956451_7488699268081717603_n_1587715122.jpg" width="130px"></td>
+        <td><a href="https://twitter.com/ppkritt?t=S4Sei_nJI5qZAtv_QkKjBA&s=09">twitter</a></td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>กฤษณภูมิ พิบูลสงคราม</td>
+        <td>เจเจ</td>
+        <td><img src="https://static.posttoday.com/media/content/2020/11/18/8B1AC0479E3059AD044F8F120A4746B4_1000.jpg" width="130px"></td>
+        <td><a href="https://twitter.com/JJksnp?t=14cHuqyBulqhdrXFuWSMGw&s=09">twitter</a></td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td>มาร์ค ต้วน</td>
+        <td>มาร์ค</td>
+        <td><img src="https://finzaaa.com/uploads/images/2021/11/image_750x_618e0a6eb8425.jpg" width="130px"></td>
+        <td><a href="https://twitter.com/marktuan?t=EDJPPrK93U_LJ1oQJ6fEOA&s=09">twitter</a></td>
+      </tr>
+      <tr>
+        <td>6</td>
+        <td>โคนัน ลี เกรย์</td>
+        <td>โคนัน</td>
+        <td><img src="https://yt3.ggpht.com/gMO0WsckiQPI6NlWKCq51VJeMxY7GWiRkBgkyPalySBVKI41LR6-Dehlj8WDm6KHY1h07C1-Puc=s900-c-k-c0x00ffffff-no-rj" width="130px"></td>
+        <td><a href="https://twitter.com/conangray?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a></td>
+      </tr>  
+    </table>
+  </body> 
+</html>
