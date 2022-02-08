54dd8c12e8f451a2827ad9d0c0c71b810daf70bd
Thitira <97937737+Thitira@users.noreply.github.com>
2022-02-08 18:09:07 +0700

    Update table.php

--- table.php
+++ table.php
@@ -21,19 +21,3 @@
     </table>
   </body> 
 </html>
-<table border="1">
-      <tr>
-        <th>ลำดับ</th>
-        <th>ชื่อ - นามสกุล</th>
-        <th>ชื่อเล่น</th>
-        <th>รูป</th>
-        <th>ทวิตเตอร์</th>
-      </tr>
-      <tr>
-        <td>1</td>
-        <td>กันต์พิมุกต์ ภูวกุล</td>
-        <td>แบมแบม</td>
-        <td><img src="https://pbs.twimg.com/profile_images/1481642811749404682/NXBKpa7n.jpg" width="130px"></td>
-        <td><a href="https://twitter.com/BamBam1A?t=xQkCquRUuOw6C-0r6Duz2Q&s=09">twitter</a></td>
-      </tr>  
-    </table>
