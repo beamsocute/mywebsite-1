a15005cd1f0c1ec761fc5727e8e10e29b46fdd94
Thitira <97937737+Thitira@users.noreply.github.com>
2022-02-08 17:51:27 +0700

    Update table.php

--- table.php
+++ table.php
@@ -15,7 +15,7 @@
         <td>1</td>
         <td>ยิ่งคุณ รอดทิม</td>
         <td>ขม</td>
-        <td><img src="https://pin.it/6XEU6QJ" width="130px"></td>
+        <td><img src="https://images.app.goo.gl/vf23pEt5h9qHQV3w6" width="130px"></td>
         <td><a href="https://twitter.com/BamBam1A?t=xQkCquRUuOw6C-0r6Duz2Q&s=09">twitter</a></td>
       </tr>  
     </table>
