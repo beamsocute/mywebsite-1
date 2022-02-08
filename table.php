eb9e648ac7d068cc94367bd280dc0a29aa3754ec
Thitira <97937737+Thitira@users.noreply.github.com>
2022-02-08 19:22:56 +0700

    Update table.php

--- table.php
+++ table.php
@@ -13,10 +13,45 @@
       </tr>
       <tr>
         <td>1</td>
-        <td>กันต์พิมุกต์ ภูวกุล</td>
-        <td>แบมแบม</td>
-        <td><img src="https://images.app.goo.gl/mtwdoz1wEfdFiwdDA" width="130px"></td>
-        <td><a href="https://twitter.com/BamBam1A?t=xQkCquRUuOw6C-0r6Duz2Q&s=09">twitter</a></td>
+        <td>มารี เออเจนี เลอเอย์</td>
+        <td>ส้ม</td>
+        <td><img src="https://s.isanook.com/jo/0/rp/r/w728/ya0xa0m1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL2pvLzAvdWQvNDg0LzI0MjA4MTMvejIuanBn.jpg" width="130px"></td>
+        <td><a href="https://twitter.com/zommarie?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a></td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>โอลิเวีย อีซาเบล ร็อดริโก</td>
+        <td>โอลิเวีย</td>
+        <td><img src="https://assets.teenvogue.com/photos/6076dcb99774944e1309a4d1/2:3/w_1000,h_1500,c_limit/olivia.jpeg" width="130px"></td>
+        <td><a href="https://twitter.com/oliviarodrigo?lang=en">twitter</a></td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>อี จีอึน</td>
+        <td>ไอยู</td>
+        <td><img src="https://www.akerufeed.com/wp-content/uploads/2021/02/iu1.png" width="130px"></td>
+        <td><a href="https://mobile.twitter.com/_iuofficial?lang=en">twitter</a></td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>อารีอานา กรานเด-บูเทรา</td>
+        <td>อารีอานา</td>
+        <td><img src="https://assets.teenvogue.com/photos/613b5fd248eda7f19679403c/2:3/w_1344,h_2017,c_limit/1235152164" width="130px"></td>
+        <td><a href="https://twitter.com/arianatoday?lang=en">twitter</a></td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td>อารี สเตแพรนส์ เลฟฟ์</td>
+        <td>เลาฟ์</td>
+        <td><img src="https://lastfm.freetls.fastly.net/i/u/770x0/114ff5c03bbc402ac0b14a20a30c8afd.jpg" width="130px"></td>
+        <td><a href="https://twitter.com/lauvsongs?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a></td>
+      </tr>
+      <tr>
+        <td>6</td>
+        <td>โคนัน ลี เกรย์</td>
+        <td>โคนัน</td>
+        <td><img src="https://yt3.ggpht.com/gMO0WsckiQPI6NlWKCq51VJeMxY7GWiRkBgkyPalySBVKI41LR6-Dehlj8WDm6KHY1h07C1-Puc=s900-c-k-c0x00ffffff-no-rj" width="130px"></td>
+        <td><a href="https://twitter.com/conangray?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">twitter</a></td>
       </tr>  
     </table>
   </body> 
