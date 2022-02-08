cb82c57f09ee211510101dc72b51c52bcaf002a2
Thitira <97937737+Thitira@users.noreply.github.com>
2022-02-08 15:16:55 +0700

    Update table.php

--- table.php
+++ table.php
@@ -1,19 +1,22 @@
-
 <html>
   <head>
     <title>ตารางของฉัน</title>
   </head>
   <body>
-    <table border="26">
+    <table border="1">
       <tr>
         <th>ลำดับ</th>
         <th>ชื่อ - นามสกุล</th>
         <th>ชื่อเล่น</th>
+        <th>รูป</th>
+        <th>ทวิตเตอร์</th>
       </tr>
       <tr>
         <td>1</td>
-        <td>ฐิติ​รัตน์​ อ่อน​ศรี​</td>
-        <td>น้ำตาล</td>
+        <td>ยิ่งคุณ รอดทิม</td>
+        <td>ขม</td>
+        <td><img src="https://asianwiki.com/images/e/e8/Song_Joong-Ki-p002.jpeg" width="130px"></td>
+        <td><a href="https://twitter.com/sjk_kith?lang=en">twitter</a></td>
       </tr>  
     </table>
   </body> 
