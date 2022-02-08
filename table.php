64a27c2f2be9857b1d3cf76136256496902c6118
Thitira <97937737+Thitira@users.noreply.github.com>
2022-02-08 18:00:24 +0700

    Update table.php

--- table.php
+++ table.php
@@ -15,7 +15,7 @@
         <td>1</td>
         <td>ยิ่งคุณ รอดทิม</td>
         <td>ขม</td>
-        <td><img src="https://s.isanook.com/ns/0/rp/rc/w850h510/yatxacm1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL25zLzAvdWQvMTY5OS84NDk4Nzg2LzE1LmpwZw==.jpg" width="130px"></td>
+        <td><img src="https://s.isanook.com/ns/0/rp/rc/.jpg" width="130px"></td>
         <td><a href="https://twitter.com/BamBam1A?t=xQkCquRUuOw6C-0r6Duz2Q&s=09">twitter</a></td>
       </tr>  
     </table>
