3bd7f770b105564248930faab97aee45f30617a8
Thitira <97937737+Thitira@users.noreply.github.com>
2022-02-22 14:04:40 +0700

    Update font.php

--- font.php
+++ font.php
@@ -1,1 +1,24 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>กำหนดรูปแบบตัวอักษร</title>
+  <style>
+    @font-face{
+      font-family: myFont1;
+      src: url(SOV_sorm2496.woff)
+    }
+    @font-face{
+      font-family: myFont2;
+      src: url(SOV_sorm2496.woff)
+    }
+    #font1{
+	font-family: myFont1;    
+    }
+  </style>
+</head>
+<body>
+  <h1 id="font1">นางสาวฐิติรัตน์ อ่อนศรี ม.4/13 เลขที่ 26 เลขประจำตัว 58526</h1>
+  <h1 id="font2">นางสาวฐิติรัตน์ อ่อนศรี ม.4/13 เลขที่ 26 เลขประจำตัว 58526</h1>
+</body>
+</html>
